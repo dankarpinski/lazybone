d66990bc9873d82a98d6f501cd685abd11ff0d2f
Daniel Karpinski <thedannyk@googlemail.com>
2015-06-04 21:52:36 +0200

    Add README and setup.py for pypi

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(name='lazybone',
+      version='0.1.0',
+      py_modules=['lazybone'],
+      description='Python Library for connecting to a Lazybone Bluetooth Relay',
+      author='Daniel Karpinski',
+      author_email='dankarpinski@hotmail.com',
+      classifiers=[
+          'Development Status :: Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          ],
+      )
